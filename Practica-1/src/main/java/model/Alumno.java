1de27fc8aecfbd6b093af97739b7d6889b0cf42a
José Gallardo <jgc1031@alu.ubu.es>
2025-05-03 12:33:25 +0200

    Agregar configuración de proyecto y clases de entidad para Prueba y Practica-1

---
+++ Practica-1/src/main/java/model/Alumno.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Alumno
+ *
+ */
+@Entity
+@Table(name="Alumno")
+public class Alumno implements Serializable {
+
+	@Id
+	private String NIF;
+	private Integer edad;
+	private String nombre;
+	private String apellido;
+	private static final long serialVersionUID = 1L;
+
+	public Alumno() {
+		super();
+	}   
+	public String getNIF() {
+		return this.NIF;
+	}
+
+	public void setNIF(String NIF) {
+		this.NIF = NIF;
+	}   
+	public Integer getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}   
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}   
+	public String getApellido() {
+		return this.apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+   
+}
