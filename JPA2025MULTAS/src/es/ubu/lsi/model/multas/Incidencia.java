c8c4825bc1b01c492860a63fc97c87ce7176160a
José Gallardo <jgc1031@alu.ubu.es>
2025-05-06 10:30:18 +0200

    clase incidencia sin terminar

--- JPA2025MULTAS/src/es/ubu/lsi/model/multas/Incidencia.java
+++ JPA2025MULTAS/src/es/ubu/lsi/model/multas/Incidencia.java
@@ -1,5 +1,88 @@
 package es.ubu.lsi.model.multas;
 
-public class Incidencia {
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
 
+/**
+ * Entity implementation class for Entity: Incidencia
+ *
+ */
+@Entity
+@Table(name="Incidencia")
+public class Incidencia implements Serializable {
+
+	@Id
+	private Date fecha;
+	@Id
+	private String NIF;
+	private String nombre;
+	private String apellido;
+	@OneToMany
+	@JoinColumn(name = "dp_ip") 
+	private DireccionPostal direccion;
+	private Integer puntos;
+	@ManyToMany
+	@JoinColumn(name = "idauto") 
+	private Vehiculo idauto;
+	private static final long serialVersionUID = 1L;
+
+	public Conductor() {
+		super();
+	}  
+	
+	public String getNIF() {
+		return this.NIF;
+	}
+
+	public void setNIF(String NIF) {
+		this.NIF = NIF;
+	}   
+	   
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}   
+	public String getApellido() {
+		return this.apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getDireccion() {
+		return this.direccion.toString();
+	}
+
+	public void setDireccion(DireccionPostal direccion) {
+		this.direccion = direccion;
+	}
+	
+	public Integer getPuntos() {
+		return this.puntos;
+	}
+
+	public void setPuntos(Integer puntos) {
+		this.puntos = puntos;
+	}
+	
+	public Vehiculo getIdauto() {
+		return this.idauto;
+	}
+
+	public void setIdauto(Vehiculo idauto) {
+		this.idauto = idauto;
+	}
+   
+   	@Override
+	public String toString() {
+		return "Conductor [NIF=" + getNIF() + ", Nombre=" + getNombre() + 
+			", Apellido=" + getApellido() + ", Direccion=" + getDireccion() + 
+			", Puntos=" + getPuntos() + ", Vehiculo=" + getIdauto() + "]";
+	}
 }
