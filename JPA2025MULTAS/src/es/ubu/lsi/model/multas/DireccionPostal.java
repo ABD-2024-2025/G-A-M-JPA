5a7d19a73fdb30fdfec1c5f797862856e8cf3367
José Gallardo <jgc1031@alu.ubu.es>
2025-05-06 10:29:08 +0200

    Agregar implementación de la clase DireccionPostal con atributos y métodos de acceso

--- JPA2025MULTAS/src/es/ubu/lsi/model/multas/DireccionPostal.java
+++ JPA2025MULTAS/src/es/ubu/lsi/model/multas/DireccionPostal.java
@@ -1,5 +1,57 @@
 package es.ubu.lsi.model.multas;
 
-public class DireccionPostal {
+import java.io.Serializable;
 
+import javax.persistence.*;
+
+@Embeddable
+public class DireccionPostal implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+
+	private Integer dp_id;
+	private String direccion;
+	private String cp;
+	private String ciudad;
+
+	public DireccionPostal() {}
+	
+	public void setDP_ID(Integer dp_id) {
+		this.dp_id = dp_id;
+	}
+	
+	public Integer getDP_ID() {
+		return this.dp_id;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public String getDireccion() {
+		return this.direccion;
+	}
+	
+	public void setCP(String cp) {
+		this.cp = cp;
+	}
+	
+	public String getCP() {
+		return this.cp;
+	}
+	
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	public String getCiudad() {
+		return this.ciudad;
+	}
+	
+   	@Override
+	public String toString() {
+		return "DireccionPostal [dp_id="+getDP_ID();
+	}
 }
+
