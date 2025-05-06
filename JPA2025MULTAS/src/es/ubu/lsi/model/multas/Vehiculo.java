350a6b99a0e82f361b76ff5c1590ef5e7468fca0
José Gallardo <jgc1031@alu.ubu.es>
2025-05-06 10:23:11 +0200

    Agregar clase modelo Vehiculo

--- JPA2025MULTAS/src/es/ubu/lsi/model/multas/Vehiculo.java
+++ JPA2025MULTAS/src/es/ubu/lsi/model/multas/Vehiculo.java
@@ -1,5 +1,53 @@
 package es.ubu.lsi.model.multas;
 
-public class Vehiculo {
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
 
+/**
+ * Entity implementation class for Entity: Vehiculo
+ *
+ */
+@Entity
+@Table(name="Vehiculo")
+
+public class Vehiculo implements Serializable {
+
+	@Id //Primary Key (PK)
+	private Integer idauto;
+	private String nombre;
+	private DireccionPostal direccion;
+	private static final long serialVersionUID = 1L;
+
+	public Vehiculo() {
+		super();
+	}   
+	
+	public Integer getIdAuto() {
+		return this.idauto;
+	}
+	public void setIdAuto(Integer idauto) {
+		this.idauto = idauto;
+	}  
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getDireccion() {
+		return this.direccion.toString();
+	}
+	public void setDireccion (DireccionPostal direccion) {
+		this.direccion = direccion;
+	}   
+   
+   	@Override
+	public String toString() {
+		return "Vehiculo [IdAuto=" + getIdAuto() + ", Nombre=" + getNombre() + 
+			", Direccion=" + getDireccion() + "]";
+	}
 }
